1b65328bc5d5b35697f7dae57dd4c2657c558832
filimon4uck <134231431+filimon4uck@users.noreply.github.com>
2024-03-06 19:52:47 +0100

    1

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,25 @@
 
 import React from "react";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-type Props = {
-  name: string;
-  age: number;
-};
-
-export const User: React.FC<Props> = ({ name, age }) => {
+function App() {
   return (
-    <div>
-      <p>`Name of user: ${name}`</p>
-      <p>`Age of user: ${age}`</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
+
+export default App;
